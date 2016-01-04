7b670b152aaa77f2514669bd3f15f6828c7ea65e
amaitland <amaitland@users.noreply.github.com>
2016-01-04 22:35:48 +1000

    Demo executing a script on frame load end (Loads html from string)

--- CefSharp.MinimalExample.Wpf/Views/MainView.xaml.cs
+++ CefSharp.MinimalExample.Wpf/Views/MainView.xaml.cs
@@ -9,6 +9,37 @@ namespace CefSharp.MinimalExample.Wpf.Views
         {
             InitializeComponent();
             DataContext = new MainViewModel();
+
+            var resourceHandlerFactory = (DefaultResourceHandlerFactory)BrowserControl.ResourceHandlerFactory;
+
+            resourceHandlerFactory.RegisterHandler("http://cefsharp.test/string.html", ResourceHandler.FromString(@"<!DOCTYPE html>
+                                    <html>
+                                    <head>
+
+                                    </head>
+                                    <body>
+                                        <div>Welcome to CefSharp!</div>
+                                        <script type='text/javascript'>
+    
+                                            function testFunction()
+                                            {
+                                                alert('test');
+                                            }
+                                        </script>
+                                    </body>
+                                    </html>", ".html"));
+
+            BrowserControl.FrameLoadEnd += OnFrameLoadEnd;
+        }
+
+        private static void OnFrameLoadEnd(object sender, FrameLoadEndEventArgs e)
+        {
+            //Only execute script for the main frame
+            if (e.Frame.IsMain)
+            {
+                //The extension methods actually invoke scripts on the main frame (I strongly recommend executing code in an anonymous closure).
+                e.Frame.EvaluateScriptAsync("(function(){ testFunction(); })();");
+            }
         }
     }
 }
