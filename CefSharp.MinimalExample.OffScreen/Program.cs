ba0fa2d728a06eb6535189660850541eac0838f2
amaitland <amaitland@users.noreply.github.com>
2016-02-28 07:40:01 +1000

    Demo persisting cache in offscreen example

--- CefSharp.MinimalExample.OffScreen/Program.cs
+++ CefSharp.MinimalExample.OffScreen/Program.cs
@@ -1,36 +1,29 @@
-﻿// Copyright © 2010-2015 The CefSharp Authors. All rights reserved.
+﻿// Copyright © 2010-2016 The CefSharp Authors. All rights reserved.
 //
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
 using System;
 using System.Diagnostics;
+using System.Drawing;
 using System.IO;
-using System.Threading;
+using System.Threading.Tasks;
 using CefSharp.OffScreen;
 
 namespace CefSharp.MinimalExample.OffScreen
 {
     public class Program
     {
-        private static ChromiumWebBrowser browser;
+        private const string TestUrl = "http://www.html-kit.com/tools/cookietester/";
 
         public static void Main(string[] args)
         {
-            const string testUrl = "https://www.google.com/";
-
-            Console.WriteLine("This example application will load {0}, take a screenshot, and save it to your desktop.", testUrl);
-            Console.WriteLine("You may see Chromium debugging output, please wait...");
+            Console.WriteLine("This example application will load {0}, take a screenshot, and save it to your desktop.", TestUrl);
             Console.WriteLine();
 
-            //Perform dependency check to make sure all relevant resources are in our output directory.
-            Cef.Initialize(new CefSettings(), shutdownOnProcessExit: true, performDependencyCheck: true);
+            // You need to replace this with your own call to Cef.Initialize();
+            Cef.Initialize(new CefSettings {CachePath = "cache1"}, false, true);
 
-            // Create the offscreen Chromium browser.
-            browser = new ChromiumWebBrowser(testUrl);
-
-            // An event that is fired when the first page is finished loading.
-            // This returns to us from another thread.
-            browser.LoadingStateChanged += BrowserLoadingStateChanged;
+            MainAsync();
 
             // We have to wait for something, otherwise the process will exit too soon.
             Console.ReadKey();
@@ -40,49 +33,72 @@ namespace CefSharp.MinimalExample.OffScreen
             Cef.Shutdown();
         }
 
-        private static void BrowserLoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
+        private static async void MainAsync()
         {
-            // Check to see if loading is complete - this event is called twice, one when loading starts
-            // second time when it's finished
-            // (rather than an iframe within the main frame).
-            if (!e.IsLoading)
+            var browserSettings = new BrowserSettings();
+            //Reduce rendering speed to one frame per second so it's easier to take screen shots
+            browserSettings.WindowlessFrameRate = 1;
+
+            using (var browser = new ChromiumWebBrowser(TestUrl, browserSettings))
             {
-                // Remove the load event handler, because we only want one snapshot of the initial page.
-                browser.LoadingStateChanged -= BrowserLoadingStateChanged;
+                await LoadPageAsync(browser);
 
-                var scriptTask = browser.EvaluateScriptAsync("document.getElementById('lst-ib').value = 'CefSharp Was Here!'");
+                // Create a new cookie
+                await browser.EvaluateScriptAsync("document.getElementsByClassName('zp')[0].submit();");
 
-                scriptTask.ContinueWith(t =>
-                {
-                    //Give the browser a little time to render
-                    Thread.Sleep(500);
-                    // Wait for the screenshot to be taken.
-                    var task = browser.ScreenshotAsync();
-                    task.ContinueWith(x =>
-                    {
-                        // Make a file to save it to (e.g. C:\Users\jan\Desktop\CefSharp screenshot.png)
-                        var screenshotPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "CefSharp screenshot.png");
+                //Wait for the page to reload
+                await LoadPageAsync(browser);
 
-                        Console.WriteLine();
-                        Console.WriteLine("Screenshot ready. Saving to {0}", screenshotPath);
-
-                        // Save the Bitmap to the path.
-                        // The image type is auto-detected via the ".png" extension.
-                        task.Result.Save(screenshotPath);
-
-                        // We no longer need the Bitmap.
-                        // Dispose it to avoid keeping the memory alive.  Especially important in 32-bit applications.
-                        task.Result.Dispose();
-
-                        Console.WriteLine("Screenshot saved.  Launching your default image viewer...");
-
-                        // Tell Windows to launch the saved image.
-                        Process.Start(screenshotPath);
-
-                        Console.WriteLine("Image viewer launched.  Press any key to exit.");			
-                    });
-                });
+                // Wait for the screenshot to be taken,
+                // if one exists ignore it, wait for a new one to make sure we have the most up to date
+                await browser.ScreenshotAsync(true).ContinueWith(DisplayBitmap);
             }
+        }
+
+        public static Task LoadPageAsync(IWebBrowser browser)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+
+            EventHandler<LoadingStateChangedEventArgs> handler = null;
+            handler = (sender, args) =>
+            {
+                //Wait for while page to finish loading not just the first frame
+                if (!args.IsLoading)
+                {
+                    browser.LoadingStateChanged -= handler;
+                    tcs.TrySetResult(true);
+                }
+            };
+
+            browser.LoadingStateChanged += handler;
+
+            return tcs.Task;
+        }
+
+        private static void DisplayBitmap(Task<Bitmap> task)
+        {
+            // Make a file to save it to (e.g. C:\Users\jan\Desktop\CefSharp screenshot.png)
+            var screenshotPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "CefSharp screenshot" + DateTime.Now.Ticks + ".png");
+
+            Console.WriteLine();
+            Console.WriteLine("Screenshot ready. Saving to {0}", screenshotPath);
+
+            var bitmap = task.Result;
+
+            // Save the Bitmap to the path.
+            // The image type is auto-detected via the ".png" extension.
+            bitmap.Save(screenshotPath);
+
+            // We no longer need the Bitmap.
+            // Dispose it to avoid keeping the memory alive.  Especially important in 32-bit applications.
+            bitmap.Dispose();
+
+            Console.WriteLine("Screenshot saved.  Launching your default image viewer...");
+
+            // Tell Windows to launch the saved image.
+            Process.Start(screenshotPath);
+
+            Console.WriteLine("Image viewer launched.  Press any key to exit.");
         }
     }
 }
